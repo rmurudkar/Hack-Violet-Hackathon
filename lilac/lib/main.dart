f47b8d77706b50d23d98a7906b4d849c1705bc14
Cathy Han <cathyhan@berkeley.edu>
2021-01-30 15:36:20 -0800

    Attempt at a custom icon

--- lilac/lib/main.dart
+++ lilac/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        resizeToAvoidBottomPadding: false,
+        resizeToAvoidBottomInset: false,
         backgroundColor: Colors.white,
         appBar: AppBar(
           centerTitle: true,
@@ -26,10 +26,45 @@ class MyApp extends StatelessWidget {
 
 class userGreeting extends StatefulWidget {
   @override
-  _userGreetingState createState() => _userGreetingState();
+  _UserGreetingState createState() => _UserGreetingState();
 }
 
-class _userGreetingState extends State<userGreeting> {
+class MoodIcons extends StatefulWidget {
+  @override
+  _MoodIconsState createState() => _MoodIconsState();
+}
+
+class _MoodIconsState extends State<MoodIcons> {
+  @override
+  Widget build(BuildContext context) {
+    // return Row(
+    //
+    // );
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        IconButton(
+          icon: ImageIcon(
+            AssetImage('assets/chill.png'),
+            size: 20,
+            color: Colors.deepPurpleAccent,),
+          // icon: Icon(Icons.volume_up),
+          tooltip: 'Chill',
+          onPressed: () {
+            // setState(() {
+            //   _volume += 10;
+            // });
+          },
+        ),
+        Text('Chill')
+      ],
+    );
+  }
+}
+
+
+class _UserGreetingState extends State<userGreeting> {
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -44,14 +79,15 @@ class _userGreetingState extends State<userGreeting> {
             style: TextStyle(fontSize: 25),
           ),
         ),
-        userFeelings(),
+        MoodIcons(),
+        UserFeelings(),
         FeelingInput(),
       ],
     );
   }
 }
 
-class userFeelings extends StatelessWidget {
+class UserFeelings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -80,7 +116,7 @@ class _FeelingInputState extends State<FeelingInput> {
       height: 230,
       width: 270,
       decoration: new BoxDecoration(
-        color: Colors.pink[100],
+        color: Colors.deepPurpleAccent[100],
         borderRadius: new BorderRadius.only(
           topLeft: const Radius.circular(20.0),
           topRight: const Radius.circular(20.0),
@@ -132,6 +168,91 @@ class _BottomBarState extends State<BottomBar> {
           title: Text("Settings", style: TextStyle(color: Colors.black)),
         ),
       ],
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
